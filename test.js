d14b80dddd31d4e9d522d365db33cf22c291ed71
Ira Miller <isysd@isysd.guld>
2019-04-15 19:11:30 +0000

    kill for iso-test 1.3

--- test.js
+++ test.js
@@ -2,5 +2,10 @@
 
 // When using import, remember to import by path. Dependencies are available in parent directory (..)
 import { finishTest } from '../iso-test/index.js'
+import './index.js'
 
+// a unit test result.
 finishTest('pass Successfully loaded module and test framework.')
+
+// call when done with all unit tests.
+finishTest('kill')
