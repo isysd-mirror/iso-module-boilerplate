04470b5dbc84c9c8c8a2e59bd0167f9317224d98
Ira Miller <isysd@iramiller.com>
2019-05-05 17:12:52 -0500

    fix node.js import

--- index.node.js
+++ index.node.js
@@ -1,4 +1,4 @@
 // Set options as a parameter, environment variable, or rc file.
 // eslint-disable-next-line no-global-assign
-require = require("../esm/esm.js")(module/* , options */)
-module.exports = require("${ESM_MAIN_NAME}")
+require = require('../esm/esm.js')(module/* , options */)
+module.exports = require('./index.js')
